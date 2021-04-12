aefd37cdd9790bb6b897b56e4c4e8c51e0d87c65
duxinhuan <1464964372@qq.com>
2021-04-12 16:39:33 +0800

    更新了index.html

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,25 @@
 $.ajaxPrefilter(function (options) {
     // console.log(options.url);
     options.url = "http://api-breakingnews-web.itheima.net"+options.url
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization : localStorage.getItem('token') || ''
+        }
+    }
+
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function ({responseJSON}) {
+        // console.log(res);
+        // 对象解构
+        const { status,message} = responseJSON
+        // 在 complete 回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
+        if(status === 1 && message === "身份认证失败！"){
+            // 强制清空 token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+        location.href = '/login.html'
+        }
+    }
 })
